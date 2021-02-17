00e7397ec18ae30237625589f0db27a865ac5128
Artem Derenok <derenokartem95@gmail.com>
2021-02-17 13:32:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let number = "";
+    n = String(n);
+    for (let i = n.length - 1; i >= 0; i--) {
+        number += n[i];
+    }
+    return number;
+};
